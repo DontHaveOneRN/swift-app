76dd294ae4cb766f7984ef54959028caf4d14a46
Paul Legaspi <donthaveonern@gmail.com>
2017-07-24 16:24:57 -1000

    My first commit

--- Swyft App/ViewController.swift
+++ Swyft App/ViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    var tapCount = 0
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+      
+        if tapCount >= 10 {
+            theLabel.text = "You have tapped this button over ten times"
+        }
+    }
+    @IBAction func ButtonPromotion(_ sender: Any) {
+        theLabel.text = "Buttons are cool!"    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
